5121fefdcd996013c16e77590b8049fe11d16848
Pavel <velesgp@mail.ru>
2017-02-02 13:43:28 +0200

    Some changes

---
+++ Week2/Task_6/Song.java
@@ -0,0 +1,39 @@
+package Week2.Task_6;
+
+/**
+ * Created by Pavel
+ */
+public class Song
+{
+    private String name;
+    private String artist;
+    private Integer rating;
+
+    public Song(String name, String artist, Integer rating)
+    {
+        this.name = name;
+        this.artist = artist;
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("| %-40s | %-30s | %-7d |", name, artist, rating);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public Integer getRating()
+    {
+        return rating;
+    }
+}
