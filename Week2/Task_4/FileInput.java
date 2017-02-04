05df72438f43d60596dd27b8163ac7b8ad6da774
Pavel <velesgp@mail.ru>
2017-02-04 14:06:39 +0200

    Test2

--- Week2/Task_4/FileInput.java
+++ Week2/Task_4/FileInput.java
@@ -1,4 +1,5 @@
 package Week2.Task_4;
+
 import java.io.*;
 import java.util.*;
 
@@ -6,43 +7,9 @@ import java.util.*;
 public class FileInput
 {
     //This method reads the file and saves the data to the LinkedHashMap, the english word is a key, the russian one is a value.
-    public static String fileRead()
+    public static BufferedReader getFileReader() throws FileNotFoundException
     {
         String filePath = "CoursesHomeTasks\\src\\Week2\\Task_4\\rsen.txt"; //This path needs to be changed
-        String line;
-        List<String> arrList = new ArrayList<String>();
-        Map<String, String> map = new LinkedHashMap<String, String>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath)))
-        {
-            while ((line = reader.readLine()) != null)
-            {
-                //Create array for storing key-value pairs and than put it in the map
-                String[] parts = line.split(":", 2);
-                if (parts.length >= 2)
-                {
-                    String value = parts[0];
-                    String key = parts[1];
-                    if (key.contains(","))
-                    {
-
-                        arrList = Arrays.asList(key.replaceAll(" ", "").split(","));
-                        for (String s: arrList)
-                        {
-                            map.put(s, value);
-                        }
-                    }
-                    else
-                    {
-                        map.put(key, value);
-                    }
-                }
-            }
-
-        }
-        catch (Exception e)
-        {
-            System.err.println("Error!" + e.getMessage());
-        }
-        return map;
+        return new BufferedReader(new FileReader(filePath));
     }
 }
